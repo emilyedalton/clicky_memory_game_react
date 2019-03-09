ad32586e12c0aaf7277b2a154c252b5cf20ea375
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2019-03-09 07:36:44 -0600

    Updates

---
+++ precache-manifest.c9ab727ba995a0c6da4c393a54f1a1f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2dc1d5b1b481c850e9f",
+    "url": "/clicky_memory_game_react/static/js/runtime~main.edef4f21.js"
+  },
+  {
+    "revision": "a2881352a745b363a5a9",
+    "url": "/clicky_memory_game_react/static/js/main.80851d9b.chunk.js"
+  },
+  {
+    "revision": "c511c8623c9d26c82b7c",
+    "url": "/clicky_memory_game_react/static/js/2.09beb314.chunk.js"
+  },
+  {
+    "revision": "a2881352a745b363a5a9",
+    "url": "/clicky_memory_game_react/static/css/main.ea2645d1.chunk.css"
+  },
+  {
+    "revision": "5445a40b8a7f07a79b9099f364f957f5",
+    "url": "/clicky_memory_game_react/index.html"
+  }
+];
